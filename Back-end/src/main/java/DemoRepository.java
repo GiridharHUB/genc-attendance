02ed4cc5c8b4affa2f3d7e16e2dcb97fd9d7366a
GiridharHUB <118665505+GiridharHUB@users.noreply.github.com>
2023-05-30 09:55:24 +0530

    Delete DemoRepository.java

--- Back-end/src/main/java/DemoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fixedcode.attendance_tracking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fixedcode.attendance_tracking.model.Attendance;
-
-public interface DemoRepository extends JpaRepository<Attendance, Integer> {
-
-}
