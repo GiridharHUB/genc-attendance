8e1593b691118925c786de4dbd7d2b503610b621
Karthik <133974074+k-a-r-t-h-i-k-07@users.noreply.github.com>
2023-06-01 11:38:22 +0530

    AttendanceService.java

--- Back-end/src/main/java/com/attendanceTracking/service/AttendanceService.java
+++ Back-end/src/main/java/com/attendanceTracking/service/AttendanceService.java
@@ -29,11 +29,22 @@ public class AttendanceService {
 	public void updateAttendance(Integer id, AttendanceModel AttendanceModel) {
 		repo.save(AttendanceModel);
 }
+	public List<AttendanceModel> getAllAttendance(){
+	return repo.findAll();
+}
+	public void addAttendance(AttendanceModel AttendanceModel) {
+		
+		repo.save(AttendanceModel);
+		
+	}
+	public void updateAttendance(Integer id, AttendanceModel AttendanceModel) {
+		AttendanceModel existingupdateAttendanceModel = repo.findById(id).orElse(null);
+		/*existingupdateAttendance.setDaily_Attendance(existingupdateAttendance.getDaily_Attendance());
+		existingupdateAttendance.setMonthly_RTO_Dates(existingupdateAttendance.getMonthly_RTO_Dates());
+		*/// TODO Auto-generated method stub
+		repo.save(AttendanceModel);
+	}
+}
 
-	
-	
-	
-	
-	
 	
 	
