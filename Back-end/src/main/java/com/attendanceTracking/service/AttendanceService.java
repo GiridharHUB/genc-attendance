f05bccf70c3bfd613a275eccfc92ca8c42b63b68
Darshan0-s <133976776+Darshan0-s@users.noreply.github.com>
2023-06-01 11:04:07 +0530

    Update AttendanceService.java

--- Back-end/src/main/java/com/attendanceTracking/service/AttendanceService.java
+++ Back-end/src/main/java/com/attendanceTracking/service/AttendanceService.java
@@ -37,35 +37,3 @@ public class AttendanceService {
 	
 	
 	
-package com.fixedcode.attendance_tracking.service;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.fixedcode.attendance_tracking.model.Attendance;
-import com.fixedcode.attendance_tracking.repository.DemoRepository;
-
-@Service
-public class DemoServvice {
-	@Autowired
-	private DemoRepository DemoRepo;
-	public List<Attendance> getAllAttendance(){
-	return DemoRepo.findAll();
-}
-	public void addAttendance(Attendance Attendance) {
-		
-		DemoRepo.save(Attendance);
-		
-	}
-	public void updateAttendance(Integer id, Attendance Attendance) {
-		Attendance existingupdateAttendance = DemoRepo.findById(id).orElse(null);
-		/*existingupdateAttendance.setDaily_Attendance(existingupdateAttendance.getDaily_Attendance());
-		existingupdateAttendance.setMonthly_RTO_Dates(existingupdateAttendance.getMonthly_RTO_Dates());
-		*/// TODO Auto-generated method stub
-		DemoRepo.save(Attendance);
-	}
-	
-}
