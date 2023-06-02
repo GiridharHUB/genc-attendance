2fe24a5951a2c5bf95dd56687851fbdef553c659
Karthik <133974074+k-a-r-t-h-i-k-07@users.noreply.github.com>
2023-06-02 11:11:30 +0530

    AttendanceController.java

--- Back-end/src/main/java/attendanceTracking/controller/AttendanceController.java
+++ Back-end/src/main/java/attendanceTracking/controller/AttendanceController.java
@@ -1,20 +1,23 @@
 package com.attendanceTracking.controller;
 
-import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import com.attendanceTracking.model.AttendanceModel;
-
+import com.attendanceTracking.model.Attendance;
 import com.attendanceTracking.service.AttendanceService;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/attendance")
 public class AttendanceController {
@@ -22,14 +25,14 @@ public class AttendanceController {
 	@Autowired
 	private AttendanceService service;
 	
-	/*
-	 * @GetMapping("/show") public List<AttendanceModel> getAll(){ return
-	 * service.showAll(); }
-	 */
-	
-	
 	@GetMapping("/{id}")
-	public Optional<AttendanceModel> getById(@PathVariable("id") Integer id){
+	public Optional<Attendance> getById(@PathVariable("id") Integer id){
 		return service.showAll(id);
 	}
+	
+	@PutMapping("/save/{id}")
+	public void updateAttendance(@PathVariable Integer id, @RequestBody Attendance AttendanceModel ) {
+		service.updateAttendance(id, AttendanceModel);
+	}
+	
 }
