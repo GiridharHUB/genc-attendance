b6524c3646356f4b2591009d24a0e36c4676cf23
Karthik <133974074+k-a-r-t-h-i-k-07@users.noreply.github.com>
2023-06-02 11:07:03 +0530

    AttendanceModel.java

--- Back-end/src/main/java/attendanceTracking/model/AttendanceModel.java
+++ Back-end/src/main/java/attendanceTracking/model/AttendanceModel.java
@@ -1,88 +1,80 @@
 package com.attendanceTracking.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Attendance_Tracking")
-public class AttendanceModel {
-	
+public class Attendance {
 	@Id
-	private int Associate_Id;
+	private Integer Associate_Id;
 	private String Associate_Name;
-	private int Project_Id;
+	private Integer Project_Id;
 	private String Project_Desc;
-	private String Base_location;
+	private String Base_Location;
 	private String Monthly_RTO_Dates;
 	private String Daily_Attendance;
 	private String Location;
-
-	public int getAssociate_Id() {
+	private String Attendance_Date;
+	
+	public Integer getAssociate_Id() {
 		return Associate_Id;
 	}
-
-	public void setAssociate_Id(int associate_Id) {
+	public void setAssociate_Id(Integer associate_Id) {
 		Associate_Id = associate_Id;
 	}
-
 	public String getAssociate_Name() {
 		return Associate_Name;
 	}
-
 	public void setAssociate_Name(String associate_Name) {
 		Associate_Name = associate_Name;
 	}
-
-	public int getProject_Id() {
+	public Integer getProject_Id() {
 		return Project_Id;
 	}
-
-	public void setProject_Id(int project_Id) {
+	public void setProject_Id(Integer project_Id) {
 		Project_Id = project_Id;
 	}
-
 	public String getProject_Desc() {
 		return Project_Desc;
 	}
-
 	public void setProject_Desc(String project_Desc) {
 		Project_Desc = project_Desc;
 	}
-
-	public String getBase_location() {
-		return Base_location;
+	public String getBase_Location() {
+		return Base_Location;
 	}
-
-	public void setBase_location(String base_location) {
-		Base_location = base_location;
+	public void setBase_Location(String base_Location) {
+		Base_Location = base_Location;
 	}
-
-
-
-	public String getDaily_Attendance() {
-		return Daily_Attendance;
-	}
-
-	public void setDaily_Attendance(String daily_Attendance) {
-		Daily_Attendance = daily_Attendance;
-	}
-
-	public String getLocation() {
-		return Location;
-	}
-
-	public void setLocation(String location) {
-		Location = location;
-	}
-
 	public String getMonthly_RTO_Dates() {
 		return Monthly_RTO_Dates;
 	}
-
 	public void setMonthly_RTO_Dates(String monthly_RTO_Dates) {
 		Monthly_RTO_Dates = monthly_RTO_Dates;
 	}
+	public String getDaily_Attendance() {
+		return Daily_Attendance;
+	}
+	public void setDaily_Attendance(String daily_Attendance) {
+		Daily_Attendance = daily_Attendance;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	public String getAttendance_Date() {
+		return Attendance_Date;
+	}
+	public void setAttendance_Date(String attendance_Date) {
+		Attendance_Date = attendance_Date;
+	}
+
+	
+	
+
 }
+
