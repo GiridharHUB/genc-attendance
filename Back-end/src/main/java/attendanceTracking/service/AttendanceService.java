0de94bed26016b912d5afa42c1b0490ef745a510
Karthik <133974074+k-a-r-t-h-i-k-07@users.noreply.github.com>
2023-06-02 11:10:07 +0530

    AttendanceService.java

--- Back-end/src/main/java/attendanceTracking/service/AttendanceService.java
+++ Back-end/src/main/java/attendanceTracking/service/AttendanceService.java
@@ -1,13 +1,19 @@
 package com.attendanceTracking.service;
 
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.attendanceTracking.model.AttendanceModel;
-
+import com.attendanceTracking.model.Attendance;
 import com.attendanceTracking.repository.AttendanceRepo;
 
 @Service
@@ -17,10 +23,36 @@ public class AttendanceService {
 	private AttendanceRepo repo;
 
 
-	
-	  public Optional<AttendanceModel> showAll(int id) { 
+	public Optional<Attendance> showAll(int id) { 
 		  return repo.findById(id); 
 		  }
-	 
-	
+
+	public List<Attendance> getAllAttendance(){
+	return repo.findAll();
 }
+	public void addAttendance(Attendance AttendanceModel) {
+		
+		repo.save(AttendanceModel);
+	}
+	
+	public void updateAttendance(Integer id, Attendance AttendanceModel) {
+	
+		Optional<Attendance> attendance = repo.findById(id);
+        attendance.ifPresent(attend -> AttendanceModel.setAssociate_Id(attend.getAssociate_Id()));
+        attendance.ifPresent(attend-> AttendanceModel.setAssociate_Name(attend.getAssociate_Name()));
+        attendance.ifPresent(attend->AttendanceModel.setBase_Location(attend.getBase_Location()));
+    //    attendance.ifPresent(attendance->AttendanceModel.setDaily_Attendance(a.getDaily_Attendance()));
+        attendance.ifPresent(attend->AttendanceModel.setLocation(attend.getLocation()));
+        attendance.ifPresent(attend->AttendanceModel.setProject_Desc(attend.getProject_Desc()));
+        attendance.ifPresent(attend->AttendanceModel.setProject_Id(attend.getProject_Id()));
+		/* Date date = new Date(); */
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/uuuu"); 
+        LocalDate localDate = LocalDate.now();
+        AttendanceModel.setAttendance_Date(dtf.format(localDate));
+        repo.save(AttendanceModel);
+	}
+}
+
+ 
+	
+	
